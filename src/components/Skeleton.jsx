165200233a97d3b4e99daa46395198c30a57ffd9
Anas5006 <anassain13@gmail.com>
2023-07-22 22:28:31 +0530

    Added New Updated

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -3,16 +3,16 @@ import ContentLoader from 'react-content-loader'
 
 const Skeleton = () => {
   return (
-    <ContentLoader 
-    speed={2}
-    width={'100%'}
-    height={'100%'}
-    backgroundColor="#e1dbdb"
-    foregroundColor="#ecebeb"
-    className='rounded-lg'
-  >
-    <rect  width="100%" height="100%"  /> 
-  </ContentLoader>
+    <ContentLoader
+      speed={2}
+      width={'100%'}
+      height={'100%'}
+      backgroundColor="#e1dbdb"
+      foregroundColor="#ecebeb"
+      className='rounded-lg'
+    >
+      <rect width="100%" height="100%" />
+    </ContentLoader>
   )
 }
 
